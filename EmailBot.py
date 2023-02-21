2456ce92cff3e24cf162714bd060b6df60d7154f
PinkPigmyPuff <67124610+PinkPigmyPuff@users.noreply.github.com>
2023-02-21 11:27:08 -0800

    Update EmailBot.py

--- EmailBot.py
+++ EmailBot.py
@@ -1,12 +1,31 @@
-import csv, smtplib, ssl
+import csv
+import smtplib
+import ssl
 
-message = """Subject: You pee/pooness
+# The message that the email bot will send
+# "Subject" and "To" are both custom, and set the respective parts of the email
+# Anything in curly braces acts as a variable which we can specify when we send the message
+message = """Subject: Merch Question
+To: {to_address}
+Hello,
 
-Hi {name}, your pee/pooness is {pee}"""
+I hope whoever is receiving this is having a great day! My name is Ben Levine and I'm currently a junior in high school. While it might seem strange considering I live nowhere close, I have become a massive fan of {name} via the internet. I have no clue if this is a possibility but it would mean a lot. Could you possibly send me a pin, hat, t-shirt, hoodie, or any other piece of merch, so I can represent you here in Los Angeles?
 
-from_address = "bennysmith.532@gmail.com"
-# to_address = ["ben.l24@k12.Xrds.org", "iloveewoks@icloud.com"]
-password = "ldyivwrybtxdlcww"
+Thank you so much for even the consideration!
+
+Thanks,
+Ben Levine
+
+Address:
+10344 Cheviot Dr
+Los Angeles, California 90064"""
+
+#from_address = "bennysmith.532@gmail.com"
+#password = "ldyivwrybtxdlcww"
+
+from_address = "benlevine005@gmail.com"
+#password = "Barklamb5%!%!"
+password = "crgtffilxsbiavvb"
 
 
 context = ssl.create_default_context()
@@ -14,10 +33,14 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(from_address, password)
     with open("test_content - Sheet1.csv") as file:
         reader = csv.reader(file)
-        next(reader)  # Skip header row
-        for name, email, pee in reader:
+        for email, name in reader:
+            print(f"Sending an email from: {from_address} to {email}, under the name {name}")
             server.sendmail(
                 from_address,
                 email,
-                message.format(name=name, pee=pee),
+                message.format(name=name, to_address=email),
             )
+
+# SENT STATUS:
+    # Everyone up to TJ news has recieved 1 botched email
+    # Up to CPAPPAS success
